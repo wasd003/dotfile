b12312c5e3c40f8b746625fb82a3aadec5c56178
wasd003 <chenhuiji2000@gmail.com>
2022-02-17 20:16:00 +0800

    add bash.sh

---
+++ base.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# install node for coc.nvim 
+curl -sL install-node.vercel.app/lts | bash 
+
+# install tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 
+
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install zsh & oh-my-zsh
+sudo apt update
+sudo apt install zsh
+chsh -s /bin/zsh # set default shell to zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh # install oh-my-zsh
