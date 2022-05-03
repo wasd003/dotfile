11a6d30c699a25e9d4eb944a14f60b4a11af6bbd
wasd003 <chenhuiji@gmail.com>
2022-05-03 21:54:13 +0800

    update

--- boot.sh
+++ boot.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-# qemu line parameter example 
+# QEMU Parameter Example
 # https://blog.katastros.com/a?ID=01700-b8d7b378-7800-4354-8201-6743cfa32f85
+
+# Network Virtualization
+# SRIOV
+# -device vfio-pci,host=5e:00.1,id=mydev0 \
+
+# Virtio-NET MQ
+# -device virtio-net-pci,netdev=vnet,vectors=10,mq=on -netdev tap,id=vnet,vhost=on,ifname=tap0,script=no,queues=4 $@ \
+
+# Virtio-NET SQ
+# -device virtio-net-pci,netdev=vnet -netdev tap,id=vnet,vhost=on,ifname=tap1,script=no $@ \
 
 kernel_path=/home/jch/Documents/guest-linux
 qemu_path=/home/jch/Documents/qemu-6.2.0
 build=/home/jch/Documents/run-linux/build
 
-sudo iptables -P FORWARD ACCEPT
+create_tapk() {
+	tap=tap$1
+	if ! [ -e /sys/class/net/$tap ]; then
+	    echo -n "Creating tap..."
+	    sudo ip tuntap add $tap mode tap user $(whoami)
+	    sudo ip link set $tap master br0
+	    sudo ip link set dev $tap up
+	fi
+}
 
-# sudo qemu-system-x86_64 \
-    #-serial tcp:localhost:44320 \
+sudo iptables -P FORWARD ACCEPT
+create_tapk 0
+create_tapk 1
 
 rm vm.log
-
-# sriov
-# -device vfio-pci,host=5e:00.1,id=mydev0 \
-
-# mq virtio-net
-# -device virtio-net-pci,netdev=vnet,vectors=10,mq=on -netdev tap,id=vnet,vhost=on,ifname=tap0,script=no,queues=4 $@ \
-
-# sq virtio-net
-# -device virtio-net-pci,netdev=vnet -netdev tap,id=vnet,vhost=on,ifname=tap1,script=no $@ \
 
 # sudo $qemu_path/build/qemu-system-x86_64 \
 sudo qemu-system-x86_64 \
