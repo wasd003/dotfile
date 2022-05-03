f315584fd2451211b9769e4760508ff0cb6d3a05
wasd003 <chenhuiji@gmail.com>
2022-05-03 21:10:07 +0800

    update

--- apt-base.sh
+++ apt-base.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 # basic package
-apt install curl
-apt install wget
-apt install iproute2
-apt install iptables
-apt install net-tools
-apt install iputils-ping
-apt install gcc
-apt install g++
-apt install python3.8
-apt install git
-apt install make
-apt install qemu-system-x86
-apt install e2fsprogs
-apt install openssh-server
-apt install sudo
-apt install kmod
-apt install memcached
-apt install libmemcached-tools
-apt install mysql-server
+sudo apt -y install \
+	curl \
+	wget \
+	iproute2 \
+	iptables \
+	net-tools \
+	iputils-ping \
+	gcc \
+	g++ \
+	python3.8 \
+	git \
+	make \
+	qemu-system-x86 \
+	e2fsprogs \
+	openssh-server \
+	sudo \
+	kmod \
+	memcached \
+	libmemcached-tools \
+	mysql-server \
 
 # install memaslap on ubuntu
 # wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
